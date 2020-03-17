012a8e24c9317b4ed7ccb5690427490d8e1b333f
Mark-Fitzgerald2 <darak.fitzgerald@gmail.com>
2020-03-17 22:36:35 +0000

    Print an image using pyplot

--- pillow.py
+++ pillow.py
@@ -2,6 +2,10 @@ from PIL import Image
 #print('Pillow Version:', PIL.__version__)
 #Version 7.0.0
 
+
+#Print details of an image
+
+"""
 # load the image
 image = Image.open('images/Regular Tackles/1.jpg')
 # summarize some details about the image
@@ -10,3 +14,22 @@ print(image.mode)
 print(image.size)
 # show the image
 image.show()
+"""
+
+
+#load and display an image with Matplotlib
+
+
+from matplotlib import image
+from matplotlib import pyplot
+
+#load image as pixel array 
+data = image.imread('images/Regular Tackles/1.jpg')
+
+#summarize shape of the pixel array
+print(data.dtype)
+print(data.shape)
+
+#display the array of pixels as an image
+pyplot.imshow(data)
+pyplot.show()
