4078f42a0aa9fc90e10dec5e3b79a317b1d01a7a
Mark-Fitzgerald2 <darak.fitzgerald@gmail.com>
2020-03-17 22:30:27 +0000

    Printed details of image to console

--- pillow.py
+++ pillow.py
@@ -1,2 +1,12 @@
-import PIL
-print('Pillow Version:', PIL.__version__)
+from PIL import Image
+#print('Pillow Version:', PIL.__version__)
+#Version 7.0.0
+
+# load the image
+image = Image.open('images/Regular Tackles/1.jpg')
+# summarize some details about the image
+print(image.format)
+print(image.mode)
+print(image.size)
+# show the image
+image.show()
